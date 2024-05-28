04d281d61c4dfe4a7bf91daedf58825e83cc8a3d
Pierre Durand <85309955+TotallyNotAB0t@users.noreply.github.com>
2024-05-28 11:31:08 +0200

    back

---
+++ DurandPierre/Sequence.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace AI_BehaviorTree_AIImplementation
+{
+    class Sequence : Node
+    {
+        protected List<Node> child;
+        public void AddChild(Node node)
+        {
+            child.Add(node);
+        }
+
+        public override void ExecuteAction()
+        {
+            int i = 0;
+            if (state == State.Running)
+            {
+                for (i = 0; i < child.Count; i++)
+                {
+                    if(child[i].State == State.Running)
+                    {
+                        break;
+                    }
+                }
+            }
+            state = State.Running;
+            for (i = 0; i < child.Count; i++)
+            {
+                child[i].ExecuteAction();
+                if (child[i].State != State.Success)
+                {
+                    state = child[i].State;
+                    return;
+                }
+            }
+            state = State.Success;
+        }
+
+        public override void Reset()
+        {
+            this.state = State.NotExecuted;
+            for (int i = 0; i < child.Count; i++)
+            {
+                child[i].Reset();
+            }
+        }
+    }
+}
