215198e3f7bbf7cb3b3b0139d91d499a9d579a23
Pierre Durand <85309955+TotallyNotAB0t@users.noreply.github.com>
2024-05-29 15:29:19 +0200

    Better slayer

--- DurandPierre/AIDecisionMaker.cs
+++ DurandPierre/AIDecisionMaker.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
-using Windows.UI.Xaml.Controls.Primitives;
 
 namespace AI_BehaviorTree_AIImplementation
 {
@@ -20,6 +19,8 @@ namespace AI_BehaviorTree_AIImplementation
         PlayerInformations target;
         NodeManager nodeManager;
         Vector3 oldPos;
+        public List<string> whiteList = new List<string> { "GodSlayer", "GOD", "KS", "Risitas" };
+        private bool modeTeam;
 
         // Ne pas utiliser cette fonction, elle n'est utile que pour le jeu qui vous Set votre Id, si vous voulez votre Id utilisez AIId
         public void SetAIId(int parAIId) { AIId = parAIId; }
@@ -35,56 +36,55 @@ namespace AI_BehaviorTree_AIImplementation
 
         public List<AIAction> ComputeAIDecision()
         {
+            List<PlayerInformations> playerInfos = AIGameWorldUtils.GetPlayerInfosList();
+            PlayerInformations myPlayerInfos = GetPlayerInfos(AIId, playerInfos);
+
             if (actionList == null)
             {
                 actionList = new List<AIAction>();
+
+                for (int i = 0; i < playerInfos.Count; i++)
+                {
+                    if (!whiteList.Contains(playerInfos[i].Name))
+                    {
+                        modeTeam = true;
+                        break;
+                    }
+                }
+
                 Selector selectorOriginel = new Selector();
-
-                // Sequence 1
-                //Sequence Run = new Sequence();
-                //Run.AddChild(new Node(DetectLowLife));
-                //Run.AddChild(new Node(RunFromEnemy));
-                Sequence ShootEnemy = new Sequence();
-
+                
                 Sequence TowardsBonus = new Sequence();
+                TowardsBonus.AddChild(new Node(Reload));
                 TowardsBonus.AddChild(new Node(WalkTowardsBonus));
                 TowardsBonus.AddChild(new Node(DashTowardsBonus));
-                TowardsBonus.AddChild(ShootEnemy);
+                TowardsBonus.AddChild(new Node(LookTowardEnemy));
+                TowardsBonus.AddChild(new Node(Shoot));
 
+                Sequence ShootEnemy = new Sequence();
+                ShootEnemy.AddChild(new Node(Reload));
                 ShootEnemy.AddChild(new Node(LookTowardEnemy));
                 ShootEnemy.AddChild(new Node(Shoot));
                 ShootEnemy.AddChild(new Node(WalkTowardsEnemy));
-                //ShootEnemy.AddChild(new Node(DashRandomly));
-                
+                ShootEnemy.AddChild(new Node(DashTowardsEnemy));
 
-
-                //ShootEnemy.AddChild(TowardsBonus);
-
-                //selectorOriginel.AddChild(Run);
                 selectorOriginel.AddChild(TowardsBonus);
                 selectorOriginel.AddChild(ShootEnemy);
+                
                 nodeManager = new NodeManager(selectorOriginel);
             }
-
-            List<PlayerInformations> playerInfos = AIGameWorldUtils.GetPlayerInfosList();
-            PlayerInformations myPlayerInfos = GetPlayerInfos(AIId, playerInfos);
 
             actionList.Clear();
 
             foreach (PlayerInformations playerInfo in playerInfos)
             {
-                if (!playerInfo.IsActive)
-                    continue;
-
-                if (playerInfo.PlayerId == myPlayerInfos.PlayerId)
-                    continue;
-
+                if (!playerInfo.IsActive) continue;
+                if (playerInfo.PlayerId == myPlayerInfos.PlayerId) continue;
+                if (modeTeam && whiteList.Contains(playerInfo.Name)) continue;
                 target = playerInfo;
                 break;
             }
-
             nodeManager.update();
-
             return actionList;
         }
 
@@ -110,24 +110,27 @@ namespace AI_BehaviorTree_AIImplementation
             return State.Success;
         }
 
+        private State LookTowardBonus()
+        {
+            List<PlayerInformations> playerInfos = AIGameWorldUtils.GetPlayerInfosList();
+            PlayerInformations myPlayerInfos = GetPlayerInfos(AIId, playerInfos);
+            if (target != null)
+            {
+                actionList.Add(new AIActionLookAtPosition(AIGameWorldUtils.GetBonusInfosList()[0].Position));
+                return State.Success;
+            }
+            return State.Failure;
+        }
+
         private State Shoot()
         {
             actionList.Add(new AIActionFire());
             return State.Success;
         }
 
-        private State JustWalk()
-        {
-            List<PlayerInformations> playerInfos = AIGameWorldUtils.GetPlayerInfosList();
-            PlayerInformations myPlayerInfos = GetPlayerInfos(AIId, playerInfos);
-
-            actionList.Add(new AIActionMoveToDestination(new Vector3(0, 0, 0)));
-            return State.Success;
-        }
-
         private State WalkTowardsEnemy()
         {
-            actionList.Add(new AIActionMoveToDestination(new Vector3(target.Transform.Position.x, target.Transform.Position.y, target.Transform.Position.z)));
+            actionList.Add(new AIActionMoveToDestination(target.Transform.Position));
             return State.Success;
         }
 
@@ -136,7 +139,19 @@ namespace AI_BehaviorTree_AIImplementation
             if (AIGameWorldUtils.GetBonusInfosList().Count > 0)
             {
                 var nextBonus = AIGameWorldUtils.GetBonusInfosList()[0];
-                actionList.Add(new AIActionMoveToDestination(new Vector3(nextBonus.Position.x, nextBonus.Position.y, nextBonus.Position.z)));
+                actionList.Add(new AIActionMoveToDestination(nextBonus.Position));
+                return State.Success;
+            }
+            return State.Failure;
+        }
+
+        private State DashTowardsEnemy()
+        {
+            List<PlayerInformations> playerInfos = AIGameWorldUtils.GetPlayerInfosList();
+            PlayerInformations myPlayerInfos = GetPlayerInfos(AIId, playerInfos);
+            if (target != null)
+            {
+                actionList.Add(new AIActionDash(target.Transform.Position - myPlayerInfos.Transform.Position));
                 return State.Success;
             }
             return State.Failure;
@@ -146,7 +161,7 @@ namespace AI_BehaviorTree_AIImplementation
         {
             List<PlayerInformations> playerInfos = AIGameWorldUtils.GetPlayerInfosList();
             PlayerInformations myPlayerInfos = GetPlayerInfos(AIId, playerInfos);
-            if (AIGameWorldUtils.GetBonusInfosList().Count > 0 && myPlayerInfos.IsDashAvailable)
+            if (AIGameWorldUtils.GetBonusInfosList().Count > 0)
             {
                 actionList.Add(new AIActionDash(AIGameWorldUtils.GetBonusInfosList()[0].Position - myPlayerInfos.Transform.Position));
                 return State.Success;
@@ -154,40 +169,51 @@ namespace AI_BehaviorTree_AIImplementation
             return State.Failure;
         }
 
-        private State DetectLowLife()
-        {
-            List<PlayerInformations> playerInfos = AIGameWorldUtils.GetPlayerInfosList();
-            PlayerInformations myPlayerInfos = GetPlayerInfos(AIId, playerInfos);
-            if (myPlayerInfos.CurrentHealth < 5)
-            {
-                return State.Success;
-            }
-            return State.Failure;
-        }
-
-        private State RunFromEnemy()
+        private State Reload()
         {
             List<PlayerInformations> playerInfos = AIGameWorldUtils.GetPlayerInfosList();
             PlayerInformations myPlayerInfos = GetPlayerInfos(AIId, playerInfos);
             if (target == null)
             {
-                actionList.Add(new AIActionDash(myPlayerInfos.Transform.Rotation * Vector3.back));
-                return State.Failure;
+                actionList.Add(new AIActionReload());
             }
             return State.Success;
         }
 
-        private State DashRandomly()
-        {
-            List<PlayerInformations> playerInfos = AIGameWorldUtils.GetPlayerInfosList();
-            PlayerInformations myPlayerInfos = GetPlayerInfos(AIId, playerInfos);
+        //private State DetectLowLife()
+        //{
+        //    List<PlayerInformations> playerInfos = AIGameWorldUtils.GetPlayerInfosList();
+        //    PlayerInformations myPlayerInfos = GetPlayerInfos(AIId, playerInfos);
+        //    if (myPlayerInfos.CurrentHealth < 5)
+        //    {
+        //        return State.Success;
+        //    }
+        //    return State.Failure;
+        //}
 
-            if (target != null)
-            {
-                actionList.Add(new AIActionDash(new Vector3(Random.Range(0, 1), 0, Random.Range(0, 1))));
-                return State.Success;
-            }
-            return State.Failure;
-        }
+        //private State RunFromEnemy()
+        //{
+        //    List<PlayerInformations> playerInfos = AIGameWorldUtils.GetPlayerInfosList();
+        //    PlayerInformations myPlayerInfos = GetPlayerInfos(AIId, playerInfos);
+        //    if (target == null)
+        //    {
+        //        actionList.Add(new AIActionDash(myPlayerInfos.Transform.Rotation * Vector3.back));
+        //        return State.Failure;
+        //    }
+        //    return State.Success;
+        //}
+
+        //private State DashRandomly()
+        //{
+        //    List<PlayerInformations> playerInfos = AIGameWorldUtils.GetPlayerInfosList();
+        //    PlayerInformations myPlayerInfos = GetPlayerInfos(AIId, playerInfos);
+
+        //    if (target != null)
+        //    {
+        //        actionList.Add(new AIActionDash(new Vector3(Random.Range(0, 1), 0, Random.Range(0, 1))));
+        //        return State.Success;
+        //    }
+        //    return State.Failure;
+        //}
     }
 }
